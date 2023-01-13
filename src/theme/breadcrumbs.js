c996fa8c76137f785e093c95c97bf6d7a217c39d
mohamedwaleed9990@gmail.com <mohamedwaleed9990@gmail.com>
2023-01-13 09:06:09 +0200

    Merged newly added theme updates

---
+++ src/theme/breadcrumbs.js
@@ -0,0 +1,51 @@
+if ($('#wikiBreadcrumb ol.breadcrumb')) {
+    var here = location.href.replace(/(\?.*)$/,'').replace(/bdfd-wiki\/(?:nightly\/)?/, '').split('/').slice(3);
+    var root = location.href.replace(/(\?.*)$/,'').split('/').slice(3);
+    if (root[1] == 'nightly') {
+        var pathToRoot = 'bdfd-wiki/nightly/'; 
+    } else {
+        var pathToRoot = 'bdfd-wiki/';
+    };
+
+
+    var parts = [{
+        text: "Foreword",
+        link: "/"
+    }];
+
+    const map = {
+        bdscript: "Functions",
+        guides: "Guides",
+        resources: "Resources",
+        callbacks: "Callbacks",
+        premium: "Premium",
+        javascript: "JavaScript"
+    };
+    
+    const names = Object.keys(map);
+
+    here.forEach((part, j) => {
+
+        // Set the correct page names
+        let pageName = map[part.toLowerCase()] ?? document.title.split('-')[0];
+
+        // Construct links for pages
+        var link = '/' + pathToRoot + here.slice(0, j + 1).join('/');
+
+        // Construct the correct append elements
+        if (names.includes(part.toLowerCase())) {
+            var appendElement = '<li><a href="' + link + '/introduction.html"> <span> ' + pageName + ' </span></a></li>';
+        } else {
+            var appendElement = '<li><a href="' + link + '"> <span> ' + pageName + ' </span></a></li>';
+        };
+
+        // Push append elements
+        $('#wikiBreadcrumb ol.breadcrumb').append(appendElement);
+        parts.push({
+            text: pageName,
+            link: link
+        });
+
+        pageName = document.title.split('-')[0];
+    })
+}
