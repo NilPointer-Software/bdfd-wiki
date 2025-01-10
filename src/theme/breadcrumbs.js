7d076876ce9f96a7befc95c34df0cb9cc3be0f4e
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2025-01-10 12:41:51 +0300

    Update breadcrumbs.js

--- src/theme/breadcrumbs.js
+++ src/theme/breadcrumbs.js
@@ -1,55 +1,34 @@
-function isLastElement(array, element) {
-   if (element === null || element === undefined) return false;
-   return array[array.length - 1] === element;
-}
-
 function getNameFromTitle() {
-   let index = document.title.indexOf('-');
-   return document.title.substring(0, index-1);
+    let index = document.title.indexOf('-');
+    return document.title.substring(0, index-1);
 }
 
 
-let root = "/";
-const domain = ["bdfd-wiki", "nightly"];
+let root = '/';
+let paths = location.pathname.split('/').slice(1);
 
-const paths = location.pathname
-    .split("/")
-    .filter(path => path && !domain.includes(path));
 
-const categories = {
-   introduction: 0,
-   callbacks: 0,
-   bdscript: 0,
-   javascript: 0,
-   guides: 0,
-   premium: 0
-};
+const removePath = ["bdfd-wiki", "nightly"];
 
-paths.forEach(path => {
-   if (domain.includes(path)) {
-      root = `/${path}/`;
-   }
+
+paths = paths.filter(path => {
+    if (removePath.includes(path)) {
+        root += `/${path}`;
+        return false;
+    }
+
+    return true;
 });
 
-document.write(`<a href="${root}">Home</a>`);
 
-let accumulatedPath = root;
+document.write(`<a href=${root}>${"Let's go Home?"}</a>`);
 
-paths.forEach(path => {
-   if (isLastElement(paths, path) && path === "introduction.html") return;
+paths.forEach((path, index) => {
+    let name = path.charAt(0).toUpperCase() + path.slice(1);
 
-   let name = path.charAt(0).toUpperCase() + path.substring(1);
-
-   if (categories.hasOwnProperty(path)) {
-      if (isLastElement(paths, path)) return;
-      accumulatedPath += `${path}/introduction.html`;
-   } else {
-      accumulatedPath += `${name}/`;
-   }
-
-   if (isLastElement(paths, path)) {
-      document.write(`<a>${getNameFromTitle()}</a>`);
-   } else {
-      document.write(`<a href="${accumulatedPath}">${name}</a>`);
-   }
+    if (index === 0) {
+        document.write(`<a href=${root + path + "/introduction.html"}>${name}</a>`)
+    } else if (index === paths.length - 1 && path !== "introduction.html") {
+        document.write(`<a>${getNameFromTitle()}</a>`);
+    }
 });
