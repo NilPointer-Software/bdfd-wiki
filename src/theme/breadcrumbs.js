136199aa4f3af92b258c2b12473d4ab963674462
da <da@da>
2025-01-10 11:23:20 +0300

    Fixed breadcrumbs

--- src/theme/breadcrumbs.js
+++ src/theme/breadcrumbs.js
@@ -1,25 +1,55 @@
-const root = "/";
-const paths = location.pathname.split("/").filter(Boolean);
+function isLastElement(array, element) {
+   if (element === null || element === undefined) return false;
+   return array[array.length - 1] === element;
+}
+
+function getNameFromTitle() {
+   let index = document.title.indexOf('-');
+   return document.title.substring(0, index-1);
+}
+
+
+let root = "/";
+const domain = ["bdfd-wiki", "nightly"];
+
+const paths = location.pathname
+    .split("/")
+    .filter(path => path && !domain.includes(path));
+
 const categories = {
    introduction: 0,
    callbacks: 0,
    bdscript: 0,
    javascript: 0,
    guides: 0,
-}
+   premium: 0
+};
 
+paths.forEach(path => {
+   if (domain.includes(path)) {
+      root = `/${path}/`;
+   }
+});
 
 document.write(`<a href="${root}">Home</a>`);
 
 let accumulatedPath = root;
 
-paths.forEach((path, index) => {
-   if (index === paths.length - 1) return;
+paths.forEach(path => {
+   if (isLastElement(paths, path) && path === "introduction.html") return;
 
-   if (Object.keys(categories).includes(path)) {
+   let name = path.charAt(0).toUpperCase() + path.substring(1);
+
+   if (categories.hasOwnProperty(path)) {
+      if (isLastElement(paths, path)) return;
       accumulatedPath += `${path}/introduction.html`;
    } else {
-      accumulatedPath += `${path}/`;
+      accumulatedPath += `${name}/`;
    }
-   document.write(`<a href="${accumulatedPath}">${path}</a>`);
+
+   if (isLastElement(paths, path)) {
+      document.write(`<a>${getNameFromTitle()}</a>`);
+   } else {
+      document.write(`<a href="${accumulatedPath}">${name}</a>`);
+   }
 });
