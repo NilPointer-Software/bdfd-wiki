f4efb9e1839409d879b88d691defeb6eb5fd4105
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2025-01-11 23:31:41 +0300

    Update breadcrumbs.js

--- src/theme/breadcrumbs.js
+++ src/theme/breadcrumbs.js
@@ -23,8 +23,8 @@ const breadcrumbConfig = {
     "callbacks": "Callbacks",
     "premium": "Premium",
     "javascript": "JavaScript",
-     "flowchart": "Flowcharts",
-    "general": null // explicitly tell to skip general
+    "flowchart": "Flowcharts",
+    "general": null // Don't touch!
 };
 
 let accumulatedPath = root;
@@ -34,35 +34,31 @@ let numLinks = 0;
 // Iterate with normal for loop for early exits
 for (let i = 0; i < paths.length; i++) {
     let path = paths[i];
-     accumulatedPath += `${path}/`;
-
-
-     if(path.endsWith(".html")) { // handle ending file names
-           if (i === paths.length - 1) { // Only include the last html if it's last
-                breadcrumbLinks.push({
-                    href: null,
-                    name: getNameFromTitle(),
-                });
-              numLinks++;
-            }
-           continue; // skip file name
-      }
-
-      if (numLinks < 2) {
-
-          if(breadcrumbConfig[path] == null) continue; // skip if config is null
-            let name = breadcrumbConfig[path] || path.charAt(0).toUpperCase() + path.slice(1);
-          
-            let href =  accumulatedPath + "introduction.html"; // default to introduction page
-            
-              breadcrumbLinks.push({
-                  href: href,
-                  name: name,
-              });
-            numLinks++;
-       }
+    accumulatedPath += `${path}/`;
     
-}
+    if(path.endsWith(".html")) { // Handle ending file names
+        if (i === paths.length - 1) { // Only include the last html if it's last
+            breadcrumbLinks.push({
+                href: null,
+                name: getNameFromTitle(),
+            });
+            numLinks++;
+        }
+        continue; // Skip breadcrumb name
+    }
+
+    if (numLinks < 2) {
+        if(breadcrumbConfig[path] == null) continue; // Skip if config is null
+            let name = breadcrumbConfig[path] || path.charAt(0).toUpperCase() + path.slice(1);
+            let href =  accumulatedPath + "introduction.html"; // Default to introduction page
+            
+            breadcrumbLinks.push({
+                href: href,
+                name: name,
+            });
+            numLinks++;
+        }    
+    }
 
 document.write(`<a href="${root}">Home</a><p>/</p>`);
 breadcrumbLinks.forEach((link, index) => {
