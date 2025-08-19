b9eaf2f4cedb7b8a06746253510db5372d0b89a6
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2025-08-19 12:03:42 +0300

    Update playground.js

--- src/theme/playground.js
+++ src/theme/playground.js
@@ -429,22 +429,19 @@ function copyText() {
 
 function saveFile() {
   const fileName = document.getElementById('name').value;
+  const finalFileName = fileName || "bdfdwikieditor";
   const fileContent = document.getElementById('editor').value;
-
-  if (!fileName) {
-    document.getElementById('error-message').textContent = "Пожалуйста, введите имя файла.";
-    return;
-  } else {
-    document.getElementById('error-message').textContent = "";
-  }
 
   const blob = new Blob([fileContent], { type: 'text/plain' });
   const a = document.createElement('a');
   const url = URL.createObjectURL(blob);
-  a.href = url;
-  a.download = fileName + '.txt';
+
+  a.href = url; 
+  a.download = finalFileName + '.txt';
+
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
+
   URL.revokeObjectURL(url);
 }
