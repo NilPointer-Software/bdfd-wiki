9ab9506fa85694b19052ebd5ba45290a7fb8f0bb
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2026-01-14 17:55:51 +0300

    Update playground.js

--- src/theme/playground.js
+++ src/theme/playground.js
@@ -1121,6 +1121,12 @@ function expandHex(short) {
   return short;
 }
 
+function generateRandomColor() {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    const randomColorResult = document.getElementById('randomColorResult');
+    randomColorResult.style.background = randomColor;
+    randomColorResult.textContent = randomColor;
+  }
 
 
 
@@ -1530,6 +1536,7 @@ document.addEventListener('DOMContentLoaded', function() {
     updateDateFromUnix();
     updateUnixTime();
 });
+
 
 
 
