8ae1e8180f3b5500360eba8ded0b69ec3121e6c8
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2026-01-19 00:16:54 +0300

    Update apply-settings.js

--- src/theme/settings/apply-settings.js
+++ src/theme/settings/apply-settings.js
@@ -216,29 +216,7 @@ function applySettings() {
 	if (!data) localStorage.setItem("json", JSON.stringify(defaultData));
 	data ??= defaultData;
 
-	const mainHue = data["theme-main"]; // Мяу
-	const theme = data["theme-name"] || "dark"; // Гав
-	const back = data["theme-bg"]
-
-	const saturation = 80;
-	const TextSaturation = (theme === "dark") ? 100 : 50;
-	const lightness = (theme === "dark") ? 50 : 60;
-	const UILightness = (theme === "dark") ? 15 : 82;
-	const GradientLightness = (theme === "dark") ? 20 : 80;
-	const BackLightness = (theme === "dark") ? 8 : 75;
-	const TextLightness = (theme === "dark") ? 90 : 50;
-	const SearchLightness = (theme === "dark") ? 20 : 70;
-
-	// Creating cute HSL colors
-	const colorTheme1 = `hsl(${mainHue}, ${saturation}%, ${lightness}%)`;
-	const colorTheme2 = `hsl(${mainHue}, ${saturation}%, ${lightness - 20}%)`;
-	const colorTheme3 = `hsl(${mainHue}, ${saturation}%, ${UILightness}%)`;
-
 	const html = document.querySelector("html");
-
-	html.style.fontFamily = data["text-font"];
-	html.style.fontSize = data["text-size"];
-	html.style.textShadow = data["text-hg"];
 
 	if (snowflakes) {
 		snowflakes.style.visibility = data["effects"];
