b9a5ccf8ae7ccbab754f1c86b9b8559e271a8115
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2026-01-24 22:19:16 +0300

    Update apply-settings.js

--- src/theme/settings/apply-settings.js
+++ src/theme/settings/apply-settings.js
@@ -1,22 +1,3 @@
-// Params
-function formatArgFlags() {
-    document.querySelectorAll('ul').forEach(ul => {
-        ul.querySelectorAll('code.hljs').forEach(code => {
-            const html = code.innerHTML;
-            
-            const updated = html.replace(
-                /\(Type:\s*([^) ]+)\s+Flag:\s*([^) ]+)\)/gi,
-                '<span id="type">$1</span> <span id="flag">$2</span>'
-            );
-            
-            if (updated !== html) {
-                code.innerHTML = updated;
-                console.log('Updated:', code);
-            }
-        });
-    });
-}
-
 // Example "Today at"
 function removeTimestamp() {
   const timestamps = document.querySelectorAll('discord-system-message[type] .discord-message-timestamp');
@@ -482,6 +463,5 @@ function applySettings() {
 document.addEventListener('DOMContentLoaded', function() {
   createAndUpdateLastEdit();
   enhanceNavigationSimple();
-  formatArgFlags();
   applySettings();
 });
