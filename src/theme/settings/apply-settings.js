06fc9e6afd830206a0a2e7f7c4a08f5e0e1591cb
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2026-01-27 18:43:32 +0300

    Update apply-settings.js

--- src/theme/settings/apply-settings.js
+++ src/theme/settings/apply-settings.js
@@ -368,7 +368,6 @@ function applySettings() {
     "language": "en",
     "text-hg": "none",
     "text-font": "Open Sans, sans-serif",
-    "effects": "hidden",
     "code-hg": {
       defaultTextHighlight: {
         color: 4288341353,
@@ -435,10 +434,6 @@ function applySettings() {
   html.style.fontFamily = data["text-font"];
   html.style.fontSize = data["text-size"];
   html.style.textShadow = data["text-hg"];
-  
-  if (snowflakes) {
-    snowflakes.style.visibility = data["effects"];
-  }
 
   document.querySelectorAll('.chapter > li.chapter-item').forEach(el => {
    if (el.querySelector('div')) {
