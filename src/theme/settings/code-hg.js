91f28b8abc9a8e2e3d10a26ca4efe2ab610293db
RainbowKey <113303649+Rainb0wKey@users.noreply.github.com>
2026-01-30 23:17:55 +0300

    Update code-hg.js

--- src/theme/settings/code-hg.js
+++ src/theme/settings/code-hg.js
@@ -97,13 +97,6 @@ function escapeHtml(unsafe) {
 		.replace(/"/g, "&quot");
 }
 
-function escapeUnsafeHtml(html) {
-	const temp = document.createElement('div');
-	temp.textContent = html;
-	const escapedText = temp.innerHTML;
-	return escapedText.replace(/<(?!\/?span\b[^>]*>)/gi, '&lt;').replace(/(?<!<\/?span\b[^>]*)>/gi, '&gt;');
-}
-
 function highlight(scheme) {
 	const codeBlocks = document.querySelectorAll("pre code");
     
@@ -144,15 +137,14 @@ function highlight(scheme) {
 		
 		let lineNumbersHtml = '';
 		for (let i = 1; i <= lineCount; i++) {
-			const safeI = String(i).replace(/[&<>"']/g, '');
-			lineNumbersHtml += `<div class="line-number" data-line-number="${safeI}">${safeI}</div>`;
+			lineNumbersHtml += `<div class="line-number" data-line-number="${i}">${i}</div>`;
 		}
 		
 		const formattedCode = lines.map(line => {
 			if (line.trim() === '') {
 				return '<div class="code-line">&nbsp;</div>';
 			}
-			return `<div class="code-line">${escapeUnsafeHtml(line)}</div>`;
+			return `<div class="code-line">${line}</div>`;
 		}).join('');
 		
 		codeBlock.innerHTML = `
